b8c36b892d12cde40ae231aa14e53aaea38dd87d
MaXXX <alenin985@gmail.com>
2026-02-22 15:06:29 +0200

    wip

--- react/src/components/BrandCard/BrandCard.jsx
+++ react/src/components/BrandCard/BrandCard.jsx
@@ -1,24 +1,24 @@
-import {Button} from "../Button/index.jsx";
-import cls from "./BrandCard.module.css"
-import {useNavigate} from "react-router-dom";
+import { Button } from '../Button/index.jsx';
+import cls from './BrandCard.module.css';
+import { useNavigate } from 'react-router-dom';
 
-export const BrandCard = ({card}) => {
-    const navigate = useNavigate();
- return (
-  <div className={cls.card}>
-     <div className={cls.cardLabels}>Year founded {card.editDate}</div>
-     <div>{card.brand}</div>
+export const BrandCard = ({ card }) => {
+  const navigate = useNavigate();
+  return (
+    <div className={cls.card}>
+      <div className={cls.cardLabels}>Year founded {card.editDate}</div>
+      <div>{card.brand}</div>
 
-      <h5 className={cls.cardTitle}>{ card.car }</h5>
+      <h5 className={cls.cardTitle}>{card.car}</h5>
 
       <div className={cls.cardModel}>
-            <label>some text</label>
-            <p className={cls.cardModels}>{card.otherCars}</p>
+        <label>some text</label>
+        <p className={cls.cardModels}>{card.otherCars}</p>
       </div>
 
       <Button onClick={() => navigate(`/brand/${card.id}`)}>Show</Button>
-  </div>
- );
-}
+    </div>
+  );
+};
 
 export default BrandCard;
--- react/src/pages/HomePage/HomePage.tsx
+++ react/src/pages/HomePage/HomePage.tsx
@@ -1,67 +1,33 @@
 import { BrandCard } from '../../components/BrandCard/BrandCard'
-
-const reactCars = [
-    {
-        id: "1",
-        brand: "Tesla",
-        car: "Model S",
-        otherCars: "Model 3, Model X, Model Y, Cybertruck",
-        resources: [
-            "https://www.tesla.com/models",
-            "https://www.tesla.com"
-        ],
-        country: "USA",
-        isNormal: false,
-        editDate: "03.02.2025, 19:49"
-    },
-    {
-        id: "2",
-        brand: "BMW",
-        car: "3 Series",
-        otherCars: "5 Series, 7 Series, X3, X5, i4",
-        resources: [
-            "https://www.bmw.com",
-            "https://www.bmw.com/en/models/3-series/sedan/overview.html"
-        ],
-        country: "Germany",
-        isNormal: true,
-        editDate: "03.02.2025, 20:25"
-    },
-    {
-        id: "3",
-        brand: "Mercedes-Benz",
-        car: "C-Class",
-        otherCars: "E-Class, S-Class, GLC, GLE, A-Class",
-        resources: [
-            "https://www.mercedes-benz.com",
-            "https://www.mercedes-benz.com/en/vehicles/passenger-cars/c-class/"
-        ],
-        country: "Germany",
-        isNormal: true,
-        editDate: "03.02.2025, 19:01"
-    },
-    {
-        id: "4",
-        brand: "Audi",
-        car: "A4",
-        otherCars: "A3, A6, Q3, Q5, e-tron",
-        resources: [
-            "https://www.audi.com",
-            "https://www.audi.com/en/models/a4.html"
-        ],
-        country: "Germany",
-        isNormal: true,
-        editDate: "21.02.2025, 20:26"
-    }
-];
-
+import { useState, useEffect } from "react";
+import { API_URL } from '../../constants'
 
 export const HomePage = () => {
+    const [brands, setBrands] = useState([]);
+
+    // @ts-ignore
+    const getBrands = async () => {
+        try {
+            const response = await fetch(`${API_URL}/brands`);
+            const brands = await response.json();
+
+            setBrands(brands)
+
+            // brands.push(...data);
+        } catch (error) {}
+    };
+
+    useEffect(() => {
+        getBrands()
+    }, [])
+
     return (
         <>
-            {reactCars.map((card, index) => (
+            {brands.map((card, index) => (
                 <BrandCard card={card} key={index} />
             ))}
+
+            <button onClick={getBrands}>Get brands</button>
         </>
     );
 
